82b5f80953507f62595d7ec91f81d87ddaf75e6a
Rodrihollone2004 <rodri.g.fretes@gmail.com>
2024-10-20 00:45:41 -0300

    Reload Time

--- Assets/Scripts/Guns/WeaponAK.cs
+++ Assets/Scripts/Guns/WeaponAK.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using TMPro;
+using System.Collections;
 
 public class WeaponAK : MonoBehaviour, IWeapon
 {
@@ -44,14 +45,21 @@ public class WeaponAK : MonoBehaviour, IWeapon
 
         if (Input.GetKeyDown(KeyCode.R))
         {
-            bulletAkCount = 20;
-            canShoot = true;
-            UpdateBulletsCount();
-
             audioSource.PlayOneShot(reloadSoundAk);
+
+            StartCoroutine(ReloadGun());
         }
     }
 
+    public IEnumerator ReloadGun()
+    {
+        canShoot = false;
+        yield return new WaitForSeconds(2.4f);
+
+        bulletAkCount = 20;
+        canShoot = true;
+        UpdateBulletsCount();
+    }
 
     public void Shoot()
     {
--- Assets/Scripts/Guns/WeaponPistol.cs
+++ Assets/Scripts/Guns/WeaponPistol.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using TMPro;
+using System.Collections;
 
 public class WeaponPistol : MonoBehaviour, IWeapon
 {
@@ -39,12 +40,20 @@ public class WeaponPistol : MonoBehaviour, IWeapon
 
         if (Input.GetKeyDown(KeyCode.R))
         {
-            bulletPistolCount = 20;
-            canShoot = true;
-            UpdateBulletsCount();
-
             audioSource.PlayOneShot(reloadSoundPistol);
+
+            StartCoroutine(ReloadGun());
         }
+    }
+
+    public IEnumerator ReloadGun()
+    {
+        canShoot = false;
+        yield return new WaitForSeconds(2.4f);
+
+        bulletPistolCount = 20;
+        canShoot = true;
+        UpdateBulletsCount();
     }
 
     public void Shoot()
