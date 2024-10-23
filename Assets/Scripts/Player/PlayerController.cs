4e6126a01a5702579bd2a83c6590113df469c370
Totiii14 <toti.gamer14@gmail.com>
2024-10-23 18:35:33 -0300

    a

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -139,7 +139,6 @@ public class PlayerController : MonoBehaviour
         {
             return characterController.isGrounded;
         }
-        set { }
     }
 
     public void AdjustCrouchHeight(float targetHeight)
