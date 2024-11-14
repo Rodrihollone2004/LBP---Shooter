24e3eec47823cd87fb05e500099f79dcbfd8bf1f
Rodrihollone2004 <rodri.g.fretes@gmail.com>
2024-11-14 03:08:46 -0300

    Delay hostage

--- Assets/Scripts/Hostage/HostageController.cs
+++ Assets/Scripts/Hostage/HostageController.cs
@@ -4,10 +4,17 @@ public class HostageController : MonoBehaviour
 {
     Transform playerBehind;
     [SerializeField] float speed;
+    [SerializeField] float stoppingDistance;
+    [SerializeField] float holdKeyTime = 4f;
+    float currentHoldKey;
+
     bool isCaught;
+
+    private Camera mainCamera;
 
     private void Start()
     {
+        mainCamera = Camera.main;
         playerBehind = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
     }
 
@@ -27,21 +34,46 @@ public class HostageController : MonoBehaviour
 
     private void FollowPlayer()
     {
-        transform.position = Vector3.MoveTowards(transform.position, playerBehind.position, speed * Time.deltaTime);
+        float distanceToPlayer = Vector3.Distance(transform.position, playerBehind.position);
+
+        if (distanceToPlayer > stoppingDistance)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, playerBehind.position, speed * Time.deltaTime);
+        }
     }
 
     private void StopFollow()
     {
-        Debug.Log("Basta");
+        //Acá se pone la lógica de lo que va a hacer el rehen cuando esté sin pickear
     }
 
     private void Inputs()
     {
-        if (Input.GetKeyDown(KeyCode.Alpha4) && !isCaught)
+        Ray ray = mainCamera.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2));
+        RaycastHit hit;
+
+        int layerMask = LayerMask.GetMask("Hostage");
+
+        if (Physics.Raycast(ray, out hit, 2f, layerMask))
         {
-            isCaught = true;
+            if (Input.GetKey(KeyCode.Alpha4) && !isCaught)
+            {
+                currentHoldKey += Time.deltaTime;
+
+                if (currentHoldKey >= holdKeyTime)
+                {
+                    Debug.Log("Rehén en manos");
+                    isCaught = true;
+                    currentHoldKey = 0f;
+                }
+            }
+            else
+            {
+                currentHoldKey = 0f;
+            }
         }
-        else if (Input.GetKeyDown(KeyCode.Alpha2) && isCaught)
+
+        else if (Input.GetKeyDown(KeyCode.Alpha4) && isCaught)
         {
             isCaught = false;
         }
