201343df15a61eb52fe23239202615c3ce6d0343
Totiii14 <toti.gamer14@gmail.com>
2024-11-28 00:34:08 -0300

    chet the boys

---
+++ Assets/Scripts/TriggerTextUI.cs
@@ -0,0 +1,35 @@
+using TMPro;
+using UnityEngine;
+
+public class TriggerTextUI : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI uiText;
+    [SerializeField] private string message = " ";
+    [SerializeField] private float displayDuration = 5f;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            ShowMessage();
+        }
+    }
+
+    private void ShowMessage()
+    {
+        if (uiText != null)
+        {
+            uiText.text = message;
+            uiText.gameObject.SetActive(true);
+            Invoke(nameof(HideMessage), displayDuration);
+        }
+    }
+
+    private void HideMessage()
+    {
+        if (uiText != null)
+        {
+            uiText.gameObject.SetActive(false);
+        }
+    }
+}
